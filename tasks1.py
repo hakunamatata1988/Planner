878a9135890a44fc349980ffb9ff859039e0728a
hakunamatata1988 <mikimiki88@tlen.pl>
2022-07-13 11:25:38 +0200

    working on activation deactivation function

--- tasks1.py
+++ tasks1.py
@@ -32,6 +32,10 @@ def insert(table, name, id = None, duration = 0, u_time = 0, parent_id = None):
                         (NULL, ?, ?, ?, '[]', ?,'[]', '', 'False')""",
                         (name, duration, u_time, parent_id)
                 )
+                con.commit()
+
+                if parent_id is not None:
+                        add_subtask(table, parent_id, cur.lastrowid)
 
 def delete(table, id ):
 # not sure how many parameters you need
@@ -54,11 +58,11 @@ def get_row(table,id):
 def add_subtask(table, id, subtask_id):
         # no checking for loops yet
         with con:
-                cur.execute(f"SELECT subtasks_id FROM {table} WHERE id = ?", (id,))
+                cur.execute(f"SELECT * FROM {table} WHERE id = ?", (id,))
                 
-                lst = eval(cur.fetchone()[0])
+                lst = eval(cur.fetchone()['subtasks_id'])
                 lst.append(subtask_id)
-                print(repr(lst))
+
                 cur.execute(f"UPDATE {table} SET subtasks_id = ? WHERE id = ?", (repr(lst),id))
 
 
@@ -89,34 +93,76 @@ def activate(table, id):
                         return
 
                 now = datetime.datetime.today()
-                new = eval(task['time_history'])+[now]
+                new = eval(task['time_history'])+[[now]]
                 cur.execute(f"UPDATE {table} SET time_history = '{repr(new)}', active = 'True' WHERE id = {id}")
 
-
+                # update state of parents
                 for id_p in get_parents(table,id):
                         parent = get_row(table,id_p)
                         if parent['active'] == "True":
                                 return # older parents should be active
-                        new = eval(parent['time_history'])+[now]
+                        new = eval(parent['time_history'])+[[now]]
                         cur.execute(f"UPDATE {table} SET time_history = '{repr(new)}', active = 'True' WHERE id = {id_p}")
 
+def get_childs(table,id):
+        # Returns the list of ids of all childes (all generations)
+
+        task = get_row(table,id)
+        if task['subtasks_id'] == '[]':
+                return []
+        else:
+                lst_id = eval(task['subtasks_id'])
+                for id_s in lst_id:
+                        lst_id += get_childs(table,id_s)
+
+                return lst_id
+        
+def deactivate(table, id):
+        with con:
+                task = get_row(table,id)
+                if task['active'] == "False":
+                        return 
+
+                now = datetime.datetime.today()
+                new = [eval(task['time_history'])[-1] +[now]]
+                cur.execute(f"UPDATE {table} SET time_history = '{repr(new)}', active = 'False' WHERE id = {id}")      
+
+                # update state of parents, can be optimize (order?)
+                for id_p in get_parents(table,id):
+                        parent = get_row(table,id_p)
+                        flag = False
+                        for id_s in eval(parent['subtasks_id']):
+                                subtask = get_row(table,id_s)
+                                if subtask['active'] == 'True':
+                                        flag = True
+                                        break
+
+                        if not flag:
+                                new = [eval(parent['time_history'])[-1]+[now]]
+                                cur.execute(f"UPDATE {table} SET time_history = '{repr(new)}', active = 'False' WHERE id = {id_p}")   
 
 
 
-# delete_table("Tasks")
-# create_table("Tasks")
-
-# insert("Tasks","new task1")
-# insert("Tasks","new task2", parent_id =1)
-# insert("Tasks","new task3", parent_id =1)
-# insert("Tasks","new task4", parent_id =2)
-# insert("Tasks","new task5")
-# insert("Tasks","new task6", parent_id =5)
-# insert("Tasks","new task7", parent_id =5)
-# insert("Tasks","new task8", parent_id =7)
+                                           
 
 
-activate("Tasks", 2)
+
+delete_table("Tasks")
+create_table("Tasks")
+
+insert("Tasks","new task1")
+insert("Tasks","new task2", parent_id =1)
+insert("Tasks","new task3", parent_id =1)
+insert("Tasks","new task4", parent_id =2)
+insert("Tasks","new task5")
+insert("Tasks","new task6", parent_id =5)
+insert("Tasks","new task7", parent_id =5)
+insert("Tasks","new task8", parent_id =7)
+table = "Tasks"
+
+activate(table, 2)
+deactivate(table,2)
+
 show_table("Tasks")
 # con.commit()
 
